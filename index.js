975653baa92606f9fab7be4d0bbfbaa61a62277c
valen-web <128190072+valen-web@users.noreply.github.com>
2023-08-18 19:25:43 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+
+function resultdos(arr, passed, lose) {
+passed = 0
+lose = 0
+    if (arr.map = 100 && arr.map >= 90) {
+        console.log("A")
+        passed 
+    }  
+    
+    if (arr.map = 89 && arr.map >= 80) {
+        console.log("B")
+    }
+
+    if (arr.map = 79 && arr.map >= 70) {
+        console.log("C")
+    }
+    if (arr.map = 69 && arr.map >= 60 ) {
+        console.log("D")
+    }
+
+    if (arr.map < 60) {
+        console.log("F");
+    }
+    
+}
+
+const notas = [100, 50, 65, 70, 85, 96, 45]
+const mostrar = resultdos(notas)
+resultdos()
+
+
